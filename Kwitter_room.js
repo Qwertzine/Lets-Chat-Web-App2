b88858fb748e799bed21e8de016cacd5faa26494
Qwertzine <139028850+Qwertzine@users.noreply.github.com>
2024-04-13 08:37:57 +0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDRdF1Uj90VUyrOjLmHuFgRLLkbyyIDj8Q",
+    authDomain: "lets-chat-web-app-b2f89.firebaseapp.com",
+    projectId: "lets-chat-web-app-b2f89",
+    storageBucket: "lets-chat-web-app-b2f89.appspot.com",
+    messagingSenderId: "413592939835",
+    appId: "1:413592939835:web:e2266dc2dbd47e59b95b1e"
+  };
+  
